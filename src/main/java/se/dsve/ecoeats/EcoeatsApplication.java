14b59f9f96620a0160c63487e1c004f48020cfbb
Hongzhi Li <bluehawana@gmail.com>
2024-04-18 11:55:35 +0200

    first commit after springboot init

---
+++ src/main/java/se/dsve/ecoeats/EcoeatsApplication.java
@@ -0,0 +1,13 @@
+package se.dsve.ecoeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoeatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoeatsApplication.class, args);
+	}
+
+}
