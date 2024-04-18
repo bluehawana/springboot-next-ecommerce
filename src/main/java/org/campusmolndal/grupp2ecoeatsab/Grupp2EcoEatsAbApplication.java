30bdcbf1638d7c1d15ea547717da973ac0ee59dc
93joabag <93joabag@gitlab.dsve.se>
2024-04-18 10:58:56 +0200

    skapa mappstruktur

---
+++ src/main/java/org/campusmolndal/grupp2ecoeatsab/Grupp2EcoEatsAbApplication.java
@@ -0,0 +1,13 @@
+package org.campusmolndal.grupp2ecoeatsab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupp2EcoEatsAbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupp2EcoEatsAbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/campusmolndal/grupp2ecoeatsab/Grupp2EcoEatsAbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.campusmolndal.grupp2ecoeatsab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupp2EcoEatsAbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
